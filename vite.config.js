2535c765c8d67d857e939229fee8dd92311ff723
nimomoe <nimohe@vip.qq.com>
2026-04-10 14:20:19 +0800

    deploy setting

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 添加这一行：使用相对路径或指定仓库名
+  base: '/my-bookmark-manager/', 
 })
